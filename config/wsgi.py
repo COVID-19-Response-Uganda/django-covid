cff8de66c5011581df4c4cc712c7b91adff5ee22
root <root@ip-172-31-50-140.ec2.internal>
2020-05-02 11:23:03 +0000

    Changed done on the running server
     changed made to server

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,14 +1,18 @@
 """
+WSGI config for mpower project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
+import os, sys
+path = '/home/www/covlibrary/django-covid/'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
